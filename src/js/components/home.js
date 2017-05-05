d3c2a1efd5f18036a128c60581ef32cff8263110
austinrovge <austinrovge@gmail.com>
2017-05-04 22:24:48 -0500

    added home image

--- src/js/components/home.js
+++ src/js/components/home.js
@@ -5,11 +5,9 @@ class Home extends Component {
         return (
             <div className="content">
                 <img className="parallax" src="assets/stockimage.png" />
-                <h1 className="home-text">
-                    Hi<br/>
-                    I am a mediocre programmer that mostly does web based development.<br/>
-                    I am going to throw up some BS parallax scrolling image of happy people and talk about how I develop innovative stuff now.
-                </h1>
+                Hi<br/>
+                I am a mediocre programmer that mostly does web based development.<br/>
+                I am going to throw up some BS parallax scrolling image of happy people and talk about how I develop innovative stuff now.
             </div>
         );
     }
