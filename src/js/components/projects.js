f4c0da66e3c9b144bff4e87654c11a188b0bdc0d
austinrovge <austinrovge@gmail.com>
2017-06-27 23:15:45 -0500

    finished projects page. site is now mostly done and will no longer need css commits (unless i decide to redo stuff which i should do

--- src/js/components/projects.js
+++ src/js/components/projects.js
@@ -4,10 +4,20 @@ export default class Projects extends Component {
     render() {
         return (
             <div className="content">
-                <h1>Most projects can be found on my <a target="_blank" href="https://github.com/austinrovge">Github</a></h1>
+                <h1><strong>Here's stuff I've worked on</strong></h1>
                 <div className="project-div">
-                    <div className="cover"></div>
-                    <img src="/assets/atc-logo.png" />
+                    <a href="https://atthecontrol.com"><h3><strong>At The Control</strong></h3></a>
+                    <p>ATC is a streaming hub for viewing <a href="https://firstinspires.org">First Robotics</a> competitions.
+                        It was developed by <a href="https://team537.org">Team 537.</a> I learned most of my web skills through this project.</p>
+                </div>
+                <div className="project-div">
+                <a href="https://github.com/austinrovge/website"><h3><strong>This Site</strong></h3></a>
+                <p>I created this site as I had the domain <a href="http://austinrovge.com">austinrovge.com</a> and wanted to learn ReactJS.
+                    So I began working on learning React and styling this graphic mess.</p>
+                </div>
+                <div className="project-div">
+                    <a href=""><h3><strong>placeholder pls ignore</strong></h3></a>
+                    <p>REEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE</p>
                 </div>
             </div>
         );
