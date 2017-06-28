baa2d34fe9a4ce27e67a5141ff63b3d3ed81031c
austinrovge <austinrovge@gmail.com>
2017-06-28 00:36:34 -0500

    _blank changes

--- src/js/components/projects.js
+++ src/js/components/projects.js
@@ -10,17 +10,17 @@ export default class Projects extends Component {
                 <div className="content">
                     <h1><strong>Here's stuff I've worked on</strong></h1>
                     <div className="project-div">
-                        <a href="https://atthecontrol.com"><h3><strong>At The Control</strong></h3></a>
-                        <p>ATC is a streaming hub for viewing <a href="https://firstinspires.org">First Robotics</a> competitions.
-                            It was developed by <a href="https://team537.org">Team 537.</a> I learned most of my web skills through this project.</p>
+                        <a target="_blank" href="https://atthecontrol.com"><h3><strong>At The Control</strong></h3></a>
+                        <p>ATC is a streaming hub for viewing <a target="_blank" href="https://firstinspires.org">First Robotics</a> competitions.
+                            It was developed by <a target="_blank" href="https://team537.org">Team 537.</a> I learned most of my web skills through this project.</p>
                     </div>
                     <div className="project-div">
-                        <a href="https://github.com/austinrovge/website"><h3><strong>This Site</strong></h3></a>
-                        <p>I created this site as I had the domain <a href="http://austinrovge.com">austinrovge.com</a> and wanted to learn ReactJS.
+                        <a target="_blank" href="https://github.com/austinrovge/website"><h3><strong>This Site</strong></h3></a>
+                        <p>I created this site as I had the domain <code>austinrovge.com</code> and wanted to learn ReactJS.
                             So I began working on learning React and styling this graphic mess.</p>
                     </div>
                     <div className="project-div">
-                        <a href=""><h3><strong>placeholder pls ignore</strong></h3></a>
+                        <a target="_blank" href=""><h3><strong>placeholder pls ignore</strong></h3></a>
                         <p>REEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE</p>
                     </div>
                 </div>
