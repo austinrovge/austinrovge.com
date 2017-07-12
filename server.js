86b4ba4e460b3deb9a7bbc0989ad989765a28790
austinrovge <austinrovge@gmail.com>
2017-07-12 01:56:22 -0500

    fixed wildcard routing problem through node server, added yarn run start command in package.json

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 
-app.get('/', function (req, res) {
+app.use(express.static(path.join(__dirname, 'src')));
+
+app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname + '/src/index.html'));
 });
 
-app.use(express.static(path.join(__dirname, 'src')));
-
 app.listen(PORT, function () {
-    console.log('listening on port ' + PORT);
+    console.log('Server listening on port: ' + PORT);
 });
