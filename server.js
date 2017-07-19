3d1a3c9827cfd8d351e6660a47ca1519a639bc51
austinrovge <austinrovge@gmail.com>
2017-07-18 22:00:45 -0500

    smaller server.js code

--- server.js
+++ server.js
@@ -2,13 +2,7 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
-let PORT;
-
-if (process.env.NODE_ENV === 'prod') {
-    PORT = 80;
-} else if (process.env.NODE_ENV === 'dev') {
-    PORT = 8080;
-}
+let PORT = process.env.PORT || 8080;
 
 app.use(express.static(path.join(__dirname, 'src')));
 
