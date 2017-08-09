08be23ab0edb1886785a2e32e67e098456576001
austinrovge <austinrovge@gmail.com>
2017-08-09 08:57:57 -0500

    added favicons

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const path = require('path');
+const favicon = require('serve-favicon');
 const express = require('express');
 const app = express();
 
 let PORT = process.env.PORT || 8080;
 
 app.use(express.static(path.join(__dirname, 'src')));
+app.use(favicon(path.join(__dirname, 'src', '/assets/favicons/favicon.ico')));
 // app.use(express.static(path.join(__dirname, 'src/assets')));
 
 app.get('*', function (req, res) {
