87478ea234d0175aa17ccaba05502c18c2d9cf35
austinrovge <austinrovge@gmail.com>
2018-06-24 13:42:03 -0500

    replaced forever package with nodemon, removed html-loader dependency

--- util/webpack.config.js
+++ util/webpack.config.js
@@ -16,14 +16,6 @@ module.exports = {
                 use: {
                     loader: "babel-loader"
                 }
-            },
-            {
-                test: /\.html$/,
-                use: [
-                    {
-                        loader: "html-loader"
-                    }
-                ]
             }
         ]
     }
