328fd4ce7fc5e975defe2d60f2c565231024afc9
austinrovge <austinrovge@gmail.com>
2018-06-23 20:00:23 -0500

    updated not-found component, removed dependency on external css file to begin converting all pages to be independent of it

--- src/js/components/not-found.js
+++ src/js/components/not-found.js
@@ -1,16 +1,12 @@
 import React, { Component } from 'react';
-import Header from './base/header/header';
-import Footer from './base/footer/footer';
 
 export default class NotFound extends Component {
     render () {
         return (
             <div>
-                <Header />
-                <div className="content">
+                <div>
                     <h1><strong>404 Not Found :(</strong></h1>
                 </div>
-                <Footer />
             </div>
         )
     }
--- src/js/index.js
+++ src/js/index.js
@@ -6,7 +6,6 @@ import Content from './components/base/content'
 import Contact from './components/contact';
 import Projects from './components/projects';
 import NotFound from './components/not-found';
-import '../css/main.scss'; //sass
 
 class App extends Component {
     render () {
@@ -17,7 +16,7 @@ class App extends Component {
                         <Route exact path="/" render={(props) => <Content content={<Home />} pathname={props.location.pathname} {...props} />} />
                         <Route exact path="/projects" render={(props) => <Content content={<Projects />} pathname={props.location.pathname} {...props} />} />
                         <Route exact path="/contact" render={(props) => <Content content={<Contact />} pathname={props.location.pathname} {...props} />} />
-                        <Route exact path="*" component={NotFound} />
+                        <Route exact path="*" render={<Content content={<NotFound />} />} />
                     </Switch>
                 </div>
             </Router>
