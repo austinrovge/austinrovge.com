bba33f560309c86c16417dcfddd54afae34575d9
austinrovge <austinrovge@gmail.com>
2018-06-23 20:04:22 -0500

    removed webpack scss compilation code

--- conf/webpack.config.js
+++ conf/webpack.config.js
@@ -1,6 +1,3 @@
-let ExtractTextPlugin = require('extract-text-webpack-plugin');
-let webpack = require('webpack');
-
 module.exports = {
     entry: {
         main: './src/js/index.js'
@@ -17,23 +14,7 @@ module.exports = {
                 query: {
                     presets: ['react', 'es2015']
                 }
-            },
-            {
-                test: /\.scss$/,
-                loaders: ExtractTextPlugin.extract('css-loader!sass-loader')
             }
         ]
-    },
-    plugins: [
-        new ExtractTextPlugin({
-            filename: './src/css/style.css',
-            allChunks: true
-        }),
-        new webpack.DefinePlugin({
-            'process.env': {
-                NODE_ENV: JSON.stringify('production')
-            }
-        }),
-        new webpack.optimize.UglifyJsPlugin()
-    ]
+    }
 };
