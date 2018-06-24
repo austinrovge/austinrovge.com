33b2e038264e9b4301089c613ed7957072bc5c3e
austinrovge <austinrovge@gmail.com>
2018-06-24 18:47:14 -0500

    moved header out of folder

--- src/js/components/base/content.js
+++ src/js/components/base/content.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Header from './header/header';
+import Header from './header';
 import Footer from './footer/footer';
 
 export default class Content extends Component {
