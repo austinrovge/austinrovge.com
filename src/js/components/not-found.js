5efb5edc31e2b97782185823ff54d2c987acbeb5
austinrovge <austinrovge@gmail.com>
2018-07-04 12:12:03 -0500

    updated 404 route to support changes

--- src/js/components/not-found.js
+++ src/js/components/not-found.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
+import Header from './base/header';
+import Footer from './base/footer';
 
 export default class NotFound extends Component {
 
     render () {
         return (
             <div>
-                <h1><strong>404 Not Found :(</strong></h1>
+                <Header />
+                <div className="content">
+                    <h1><strong>404 Not Found :(</strong></h1>
+                </div>
+                <Footer />
             </div>
         )
     }
