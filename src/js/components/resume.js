fd03a8a403cac9a8538181e980639b9ea6af1fd4
austinrovge <austinrovge@gmail.com>
2018-07-04 21:49:08 -0500

    small text change

--- src/js/components/resume.js
+++ src/js/components/resume.js
@@ -10,7 +10,7 @@ export default class Home extends Component {
                 <Header />
                 <div className="content">
                     <h1><strong>I like being hired.</strong></h1>
-                    <p>This might not be the most up to date version of my resume, so beware! <a href="/assets/resume.pdf" target="_blank">Click here to view the raw PDF.</a></p>
+                    <p>This might not be the most up to date version of my resume, so beware! <a href="/assets/resume.pdf" target="_blank">Click here to view the PDF.</a></p>
                     <object data="/assets/resume.pdf" type="application/pdf">
                         <p>Looks like there's something wrong with the embedded PDF, <a href="/assets/resume.pdf" target="_blank">click here to view my resume!</a></p>
                     </object>
