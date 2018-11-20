bb085ab365e1ee9820bebba26b2d799e1d878021
Austin Rovge <rovgea@msoe.edu>
2018-11-20 16:37:11 -0600

    removed favicon dependency

--- src/server/server.js
+++ src/server/server.js
@@ -1,5 +1,4 @@
 import path from 'path'
-import favicon from 'serve-favicon'
 import express from 'express'
 import routes from './routes'
 
@@ -11,7 +10,6 @@ routes(app, router)
 app.use(router)
 app.use('/', express.static(__dirname))
 app.use(express.static(path.join(__dirname, '/../client')))
-// app.use(favicon(path.join(__dirname, 'src', '/assets/favicons/favicon.ico')))
 app.use('/favicon.ico', express.static(path.join(__dirname, '/../client/assets/favicons/favicon.ico')))
 
 app.get('*', (req, res) => {
