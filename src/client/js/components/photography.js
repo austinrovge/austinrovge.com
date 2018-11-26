41056f161f761f3739f71abcb3ba770c7f84f18a
Austin Rovge <rovgea@msoe.edu>
2018-11-25 22:47:41 -0600

    added photos, photo styling, and content on the photography react component

--- src/client/js/components/photography.js
+++ src/client/js/components/photography.js
@@ -7,7 +7,10 @@ export default class Photography extends Component {
 		return (
 			<Content className="content">
 				<h1><strong>I like taking pictures.</strong></h1>
-				<p>This section will have more soon!</p>
+				<p>I've been busy working on other features, so there isn't much here. I'll be able to add more content soon!</p>
+				<img className="photo" src="/assets/kepler.jpg" />
+				<img className="photo" src="/assets/mke.jpg" />
+				<img className="photo" src="/assets/kammi.jpg" />
 			</Content>
 		)
 	}
