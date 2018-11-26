7e522551cba55c725928567d6b2926570a7cb5f9
Austin Rovge <rovgea@msoe.edu>
2018-11-25 22:28:32 -0600

    updated about route with new text

--- src/client/js/components/about.js
+++ src/client/js/components/about.js
@@ -9,11 +9,11 @@ export default class About extends Component {
 				<h1><strong>Hi, I'm Austin.</strong></h1>
 				<img className="me-img" src="/assets/me.jpg" />
 				<div>
-					<p>I occasionally enjoy programming.</p>
-					<p>I first got a taste in middle school when I discovered the <a href="https://www.codecademy.com/learn/introduction-to-javascript" target="_blank">Codeacademy course for JavaScript</a>. I remember being so happy learning JavaScript for loops.</p>
-					<p>In high school, I joined robotics to dabble in C++ but really got into web development here. I helped work on <a href="https://atthecontrol.com" target="_blank">At The Control</a>, a streaming service for <a href="https://www.firstinspires.org/robotics/frc" target="_blank">FIRST Robotics Competition</a> events.</p>
-					<p>I then graduated from high school and went to the <a href="https://www.msoe.edu/" target="_blank">Milwaukee School of Engineering</a> to major in software engineering.</p>
-					<p>This website has a few projects I've worked on as well as other fun content from me.</p>
+					<p>I'm a college student who's trying to learn as much as I can about web development and programming!</p>
+					<p>I first got a taste of programming in middle school when I discovered the <a href="https://www.codecademy.com/learn/introduction-to-javascript" target="_blank">Codeacademy course for JavaScript</a>. I remember proudly showing my mom that I could use for loops in JavaScript!</p>
+					<p>In high school, I joined robotics to program robots with C++, but I ended up switching gears and helping out with the team's web presence. I helped work on <a href="https://atthecontrol.com" target="_blank">At The Control</a>, a streaming service for <a href="https://www.firstinspires.org/robotics/frc" target="_blank">FIRST Robotics Competition</a> events.</p>
+					<p>After graduating from high school, I went to the <a href="https://www.msoe.edu/" target="_blank">Milwaukee School of Engineering</a> to major in software engineering.</p>
+					<p>This website has some information about me and projects I've worked on, but I mostly use it to try out new web technologies!</p>
 				</div>
 			</Content>
 		)
