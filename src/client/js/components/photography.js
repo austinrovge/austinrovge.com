96d37dec13d3dd40fc3510f74f30ea3765b8fc08
Austin Rovge <rovgea@msoe.edu>
2018-11-25 23:09:27 -0600

    added container to fix styling issues on mobile

--- src/client/js/components/photography.js
+++ src/client/js/components/photography.js
@@ -8,9 +8,11 @@ export default class Photography extends Component {
 			<Content className="content">
 				<h1><strong>I like taking pictures.</strong></h1>
 				<p>I've been busy working on other features, so there isn't much here. I'll be able to add more content soon!</p>
-				<img className="photo" src="/assets/mke.jpg" />
-				<img className="photo" src="/assets/kepler.jpg" />
-				<img className="photo" src="/assets/kammi.jpg" />
+				<div className="photos">
+					<img className="photo" src="/assets/mke.jpg" />
+					<img className="photo" src="/assets/kepler.jpg" />
+					<img className="photo" src="/assets/kammi.jpg" />
+				</div>
 			</Content>
 		)
 	}
