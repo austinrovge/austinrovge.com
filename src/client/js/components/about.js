632a7774242763669369ac4d2e6bf0d95b5cf1e7
Austin Rovge <rovgea@msoe.edu>
2018-11-29 21:16:22 -0600

    updated about page content and updated the resume. also updated photography page and styling

--- src/client/js/components/about.js
+++ src/client/js/components/about.js
@@ -10,8 +10,8 @@ export default class About extends Component {
 				<img className="me-img" src="/assets/me.jpg" />
 				<div>
 					<p>I'm a college student who's trying to learn as much as I can about web development and programming!</p>
-					<p>I first got a taste of programming in middle school when I discovered the <a href="https://www.codecademy.com/learn/introduction-to-javascript" target="_blank">Codeacademy course for JavaScript</a>. I remember proudly showing my mom that I could use for loops in JavaScript!</p>
-					<p>In high school, I joined robotics to program robots with C++, but I ended up switching gears and helping out with the team's web presence. I helped work on <a href="https://atthecontrol.com" target="_blank">At The Control</a>, a streaming service for <a href="https://www.firstinspires.org/robotics/frc" target="_blank">FIRST Robotics Competition</a> events.</p>
+					<p>I first got a taste of programming in middle school when I discovered the <a href="https://www.codecademy.com/learn/introduction-to-javascript" target="_blank">Codeacademy course for JavaScript</a>. Within an hour of finding the course, I was proudly showing my mom that I was a "programmer" with my JavaScript for loops!</p>
+					<p>In high school, I joined robotics to program robots with C++, but I ended up switching gears and helping out with the team's web department. I helped work on <a href="https://atthecontrol.com" target="_blank">At The Control</a>, a streaming service for <a href="https://www.firstinspires.org/robotics/frc" target="_blank">FIRST Robotics Competition</a> events.</p>
 					<p>After graduating from high school, I went to the <a href="https://www.msoe.edu/" target="_blank">Milwaukee School of Engineering</a> to major in software engineering.</p>
 					<p>This website has some information about me and projects I've worked on, but I mostly use it to try out new web technologies!</p>
 				</div>
