191c737a7f954bd37c9194af6501e55e1599b2ae
Austin Rovge <rovgea@msoe.edu>
2018-11-29 23:35:22 -0600

    updated resume page text

--- src/client/js/components/resume.js
+++ src/client/js/components/resume.js
@@ -6,7 +6,7 @@ export default class Home extends Component {
 	render() {
 		return (
 			<Content className="content">
-				<h1><strong>I like being hired.</strong></h1>
+				<h1><strong>I'm working hard to learn new skills.</strong></h1>
 				<p>This might not be the most up to date version of my resume, so beware! <a href="/assets/resume.pdf" target="_blank">Click here to view the PDF.</a></p>
 				<object data="/assets/resume.pdf" type="application/pdf">
 					<p>Looks like there's something wrong with the embedded PDF, <a href="/assets/resume.pdf" target="_blank">click here to view my resume!</a></p>
